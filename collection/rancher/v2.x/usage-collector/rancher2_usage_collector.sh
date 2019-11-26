01a5b7feb685564fa87a9469d98fa1ef3da40195
dkeightley <derekjkeightley@gmail.com>
2019-11-26 16:05:52 +1300

    Add consistency for variable references

--- collection/rancher/v2.x/usage-collector/rancher2_usage_collector.sh
+++ collection/rancher/v2.x/usage-collector/rancher2_usage_collector.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "Report run on `date`"
-echo ""
+echo
 
 # Get docker id for rancher single node install
 DOCKER_ID=$(docker ps | grep "rancher/rancher:" | cut -d' ' -f1)
 
-if [ -z "$DOCKER_ID" ]
+if [ -z "${DOCKER_ID}" ]
 then
   # Get docker id for rancher ha install
   DOCKER_ID=$(docker ps | grep "k8s_rancher_rancher" | cut -d' ' -f1 | head -1)
 
-  if [ -z "$DOCKER_ID" ]
+  if [ -z "${DOCKER_ID}" ]
   then
     echo "Could not find Rancher 2 container, exiting..."
     exit -1
@@ -20,7 +20,7 @@ fi
 
 echo "Rancher version: $(docker exec ${DOCKER_ID} kubectl get settings server-version --no-headers -o custom-columns=version:value)"
 echo "Rancher id: $(docker exec ${DOCKER_ID} kubectl get settings install-uuid --no-headers -o custom-columns=id:value)"
-echo ""
+echo
 
 docker exec ${DOCKER_ID} kubectl get clusters -o custom-columns=Cluster\ Id:metadata.name,Name:spec.displayName,K8s\ Version:status.version.gitVersion,Provider:status.driver,Created:metadata.creationTimestamp,Nodes:status.appliedSpec.rancherKubernetesEngineConfig.nodes[*].address
 
@@ -29,12 +29,12 @@ CLUSTER_IDS=$(docker exec ${DOCKER_ID} kubectl get clusters --no-headers -o cust
 for ID in $CLUSTER_IDS
 do
   CLUSTER_NAME=$(docker exec ${DOCKER_ID} kubectl get cluster ${ID} --no-headers -o custom-columns=name:spec.displayName)
-  NODE_COUNT=$(docker exec ${DOCKER_ID} kubectl get nodes.management.cattle.io -n $ID --no-headers 2>/dev/null | wc -l )
+  NODE_COUNT=$(docker exec ${DOCKER_ID} kubectl get nodes.management.cattle.io -n ${ID} --no-headers 2>/dev/null | wc -l )
   ((TOTAL_NODE_COUNT += NODE_COUNT))
-  echo ""
+  echo
   echo "--------------------------------------------------------------------------------"
   echo "Cluster: ${CLUSTER_NAME} (${ID})"
-  docker exec ${DOCKER_ID} kubectl get nodes.management.cattle.io -n $ID -o custom-columns=Node\ Id:metadata.name,Address:status.internalNodeStatus.addresses[*].address,Role:status.rkeNode.role[*],CPU:status.internalNodeStatus.capacity.cpu,RAM:status.internalNodeStatus.capacity.memory,OS:status.dockerInfo.OperatingSystem,Docker\ Version:status.dockerInfo.ServerVersion,Created:metadata.creationTimestamp
+  docker exec ${DOCKER_ID} kubectl get nodes.management.cattle.io -n ${ID} -o custom-columns=Node\ Id:metadata.name,Address:status.internalNodeStatus.addresses[*].address,Role:status.rkeNode.role[*],CPU:status.internalNodeStatus.capacity.cpu,RAM:status.internalNodeStatus.capacity.memory,OS:status.dockerInfo.OperatingSystem,Docker\ Version:status.dockerInfo.ServerVersion,Created:metadata.creationTimestamp
   echo "Node count: ${NODE_COUNT}"
 done
 echo "--------------------------------------------------------------------------------"
