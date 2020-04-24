210639bfbf548378d4049b2209918fe6afb3fca8
Ben Healey <bentastic27@gmail.com>
2020-04-24 15:06:45 -0400

    Added weave to the directories to remove

--- extended-rancher-2-cleanup/extended-cleanup-rancher2.sh
+++ extended-rancher-2-cleanup/extended-cleanup-rancher2.sh
@@ -7,7 +7,7 @@ docker rm -f $(docker ps -qa)
 docker rmi -f $(docker images -q)
 docker volume rm $(docker volume ls -q)
 for mount in $(mount | grep tmpfs | grep '/var/lib/kubelet' | awk '{ print $3 }') /var/lib/kubelet /var/lib/rancher; do umount $mount; done
-cleanupdirs="/etc/ceph /etc/cni /etc/kubernetes /opt/cni /opt/rke /run/secrets/kubernetes.io /run/calico /run/flannel /var/lib/calico /var/lib/etcd /var/lib/cni /var/lib/kubelet /var/lib/rancher/rke/log /var/log/containers /var/log/pods /var/run/calico"
+cleanupdirs="/etc/ceph /etc/cni /etc/kubernetes /opt/cni /opt/rke /run/secrets/kubernetes.io /run/calico /run/flannel /var/lib/calico /var/lib/weave /var/lib/etcd /var/lib/cni /var/lib/kubelet /var/lib/rancher/rke/log /var/log/containers /var/log/pods /var/run/calico"
 for dir in $cleanupdirs; do
   echo "Removing $dir"
   rm -rf $dir
