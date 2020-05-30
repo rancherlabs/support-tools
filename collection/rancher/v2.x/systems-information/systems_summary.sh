945de3ab4d581ce7f9468d2f915bcd3e4872ac0c
David Noland <dnoland@rancher.com>
2020-05-29 21:49:50 -0700

    Fixed Rancher System Summary Report on k3s. Fixed issue with settings CRD clashing with longhorn settings.

--- collection/rancher/v2.x/systems-information/systems_summary.sh
+++ collection/rancher/v2.x/systems-information/systems_summary.sh
@@ -5,38 +5,44 @@ echo "=============================="
 echo "Run on `date`"
 echo
 
-# Get docker id for rancher single node install
-DOCKER_ID=$(docker ps | grep "rancher/rancher:" | cut -d' ' -f1)
-
-if [ -z "${DOCKER_ID}" ]
+if $(command -v k3s >/dev/null 2>&1)
 then
-  # Get docker id for rancher ha install
-  DOCKER_ID=$(docker ps | grep "k8s_rancher_rancher" | cut -d' ' -f1 | head -1)
+  KUBECTL_CMD="k3s kubectl"
+else
+  # Get docker id for rancher single node install
+  DOCKER_ID=$(docker ps | grep "rancher/rancher:" | cut -d' ' -f1)
 
   if [ -z "${DOCKER_ID}" ]
   then
-    echo "Could not find Rancher 2 container, exiting..."
-    exit -1
+    # Get docker id for rancher ha install
+    DOCKER_ID=$(docker ps | grep "k8s_rancher_rancher" | cut -d' ' -f1 | head -1)
+
+    if [ -z "${DOCKER_ID}" ]
+    then
+      echo "Could not find Rancher 2 container, exiting..."
+      exit -1
+     fi
   fi
+  KUBECTL_CMD="docker exec ${DOCKER_ID} kubectl"
 fi
 
-echo "Rancher version: $(docker exec ${DOCKER_ID} kubectl get settings server-version --no-headers -o custom-columns=version:value)"
-echo "Rancher id: $(docker exec ${DOCKER_ID} kubectl get settings install-uuid --no-headers -o custom-columns=id:value)"
+echo "Rancher version: $(${KUBECTL_CMD} get settings.management.cattle.io server-version --no-headers -o custom-columns=version:value)"
+echo "Rancher id: $(${KUBECTL_CMD} get settings.management.cattle.io install-uuid --no-headers -o custom-columns=id:value)"
 echo
 
-docker exec ${DOCKER_ID} kubectl get clusters -o custom-columns=Cluster\ Id:metadata.name,Name:spec.displayName,K8s\ Version:status.version.gitVersion,Provider:status.driver,Created:metadata.creationTimestamp,Nodes:status.appliedSpec.rancherKubernetesEngineConfig.nodes[*].address
+${KUBECTL_CMD} get clusters -o custom-columns=Cluster\ Id:metadata.name,Name:spec.displayName,K8s\ Version:status.version.gitVersion,Provider:status.driver,Created:metadata.creationTimestamp,Nodes:status.appliedSpec.rancherKubernetesEngineConfig.nodes[*].address
 
-CLUSTER_IDS=$(docker exec ${DOCKER_ID} kubectl get clusters --no-headers -o custom-columns=id:metadata.name)
+CLUSTER_IDS=$(${KUBECTL_CMD} get clusters --no-headers -o custom-columns=id:metadata.name)
 
 for ID in $CLUSTER_IDS
 do
-  CLUSTER_NAME=$(docker exec ${DOCKER_ID} kubectl get cluster ${ID} --no-headers -o custom-columns=name:spec.displayName)
-  NODE_COUNT=$(docker exec ${DOCKER_ID} kubectl get nodes.management.cattle.io -n ${ID} --no-headers 2>/dev/null | wc -l )
+  CLUSTER_NAME=$(${KUBECTL_CMD} get cluster ${ID} --no-headers -o custom-columns=name:spec.displayName)
+  NODE_COUNT=$(${KUBECTL_CMD} get nodes.management.cattle.io -n ${ID} --no-headers 2>/dev/null | wc -l )
   ((TOTAL_NODE_COUNT += NODE_COUNT))
   echo
   echo "--------------------------------------------------------------------------------"
   echo "Cluster: ${CLUSTER_NAME} (${ID})"
-  docker exec ${DOCKER_ID} kubectl get nodes.management.cattle.io -n ${ID} -o custom-columns=Node\ Id:metadata.name,Address:status.internalNodeStatus.addresses[*].address,Role:status.rkeNode.role[*],CPU:status.internalNodeStatus.capacity.cpu,RAM:status.internalNodeStatus.capacity.memory,OS:status.dockerInfo.OperatingSystem,Docker\ Version:status.dockerInfo.ServerVersion,Created:metadata.creationTimestamp
+  ${KUBECTL_CMD} get nodes.management.cattle.io -n ${ID} -o custom-columns=Node\ Id:metadata.name,Address:status.internalNodeStatus.addresses[*].address,Role:status.rkeNode.role[*],CPU:status.internalNodeStatus.capacity.cpu,RAM:status.internalNodeStatus.capacity.memory,OS:status.dockerInfo.OperatingSystem,Docker\ Version:status.dockerInfo.ServerVersion,Created:metadata.creationTimestamp
   echo "Node count: ${NODE_COUNT}"
 done
 echo "--------------------------------------------------------------------------------"
