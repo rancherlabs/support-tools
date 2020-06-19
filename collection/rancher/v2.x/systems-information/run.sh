91b549d417254569b2e8c271f5082b705dfb6484
Matthew Mattox <matthew.mattox@rancher.com>
2020-06-18 21:38:57 -0500

    Adding systems-info workload and chart

--- collection/rancher/v2.x/systems-information/run.sh
+++ collection/rancher/v2.x/systems-information/run.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 set -e
 
-if [[ -z $SMTP_USER ]]
+if [[ -z $smtp_user ]]
 then
 cat << EOF > /etc/msmtprc
 account default
-host ${SMTP_HOST}
-port ${SMTP_PORT}
-from ${FROM_ADDRESS}
+host ${smtp_host}
+port ${smtp_port}
+from ${from_address}
 logfile /var/log/msmtp.log
 EOF
 
 else
 cat << EOF > /etc/msmtprc
 account default
-host ${SMTP_HOST}
-port ${SMTP_PORT}
+host ${smtp_host}
+port ${smtp_port}
 tls on
 tls_starttls on
 tls_certcheck off
 auth on
-user ${SMTP_USER}
-password ${SMTP_PASS}
-from ${FROM_ADDRESS}
+user ${smtp_user}
+password ${smtp_pass}
+from ${from_address}
 logfile /var/log/msmtp.log
 EOF
 fi
@@ -31,9 +31,12 @@ chmod 600 /etc/msmtprc
 echo "Running Summary Report..."
 /usr/bin/systems_summary.sh | tee report.txt
 
-echo "To: ${TO_ADDRESS}" > email.txt
-echo "CC: support@support.tools" >> email.txt
-echo "From: ${FROM_ADDRESS}" >> email.txt
-echo "Subject: Rancher Systems Summary Report - ${RANCHER_NAME}" >> email.txt
+echo "To: ${to_address}" > email.txt
+if [[ "$send_to_support" == "true" ]]
+then
+  echo "CC: support@support.tools" >> email.txt
+fi
+echo "From: ${from_address}" >> email.txt
+echo "Subject: Rancher Systems Summary Report - ${rancher_name}" >> email.txt
 cat report.txt >> email.txt
-cat email.txt | msmtp -a default ${TO_ADDRESS}
+cat email.txt | msmtp -a default ${to_address}
--- collection/rancher/v2.x/systems-information/systems_summary.sh
+++ collection/rancher/v2.x/systems-information/systems_summary.sh
@@ -7,7 +7,8 @@ echo
 
 if [[ ! -z $KUBERNETES_PORT ]];
 then
-  KUBECTL_CMD="kubectl"
+  RANCHER_POD=$(kubectl get pods -l app=rancher --no-headers -o custom-columns=id:metadata.name | head -n1)
+  KUBECTL_CMD="kubectl exec ${RANCHER_POD} -- kubectl"
 else
   if $(command -v k3s >/dev/null 2>&1)
   then
@@ -33,13 +34,13 @@ echo "Rancher version: $(${KUBECTL_CMD} get settings.management.cattle.io server
 echo "Rancher id: $(${KUBECTL_CMD} get settings.management.cattle.io install-uuid --no-headers -o custom-columns=id:value)"
 echo
 
-${KUBECTL_CMD} get clusters -o custom-columns=Cluster\ Id:metadata.name,Name:spec.displayName,K8s\ Version:status.version.gitVersion,Provider:status.driver,Created:metadata.creationTimestamp,Nodes:status.appliedSpec.rancherKubernetesEngineConfig.nodes[*].address
+${KUBECTL_CMD} get clusters.management.cattle.io -o custom-columns=Cluster\ Id:metadata.name,Name:spec.displayName,K8s\ Version:status.version.gitVersion,Provider:status.driver,Created:metadata.creationTimestamp,Nodes:status.appliedSpec.rancherKubernetesEngineConfig.nodes[*].address
 
-CLUSTER_IDS=$(${KUBECTL_CMD} get clusters --no-headers -o custom-columns=id:metadata.name)
+CLUSTER_IDS=$(${KUBECTL_CMD} get cluster.management.cattle.io --no-headers -o custom-columns=id:metadata.name)
 
 for ID in $CLUSTER_IDS
 do
-  CLUSTER_NAME=$(${KUBECTL_CMD} get cluster ${ID} --no-headers -o custom-columns=name:spec.displayName)
+  CLUSTER_NAME=$(${KUBECTL_CMD} get cluster.management.cattle.io ${ID} --no-headers -o custom-columns=name:spec.displayName)
   NODE_COUNT=$(${KUBECTL_CMD} get nodes.management.cattle.io -n ${ID} --no-headers 2>/dev/null | wc -l )
   ((TOTAL_NODE_COUNT += NODE_COUNT))
   echo
