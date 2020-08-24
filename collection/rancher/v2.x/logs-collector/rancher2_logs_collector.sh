654719f5b8ae4bdb2cbb3ee0b4d3c4de4bc796b6
Matthew Mattox <matthew.mattox@rancher.com>
2020-08-24 01:32:27 -0500

    Adding sed to redact Rancher agent token

--- collection/rancher/v2.x/logs-collector/rancher2_logs_collector.sh
+++ collection/rancher/v2.x/logs-collector/rancher2_logs_collector.sh
@@ -279,7 +279,7 @@ docker-rancher() {
 
   for RANCHERAGENT in $RANCHERAGENTS; do
     docker inspect $RANCHERAGENT > $TMPDIR/rancher/containerinspect/agent-$RANCHERAGENT 2>&1
-    docker logs $SINCE_FLAG -t $RANCHERAGENT > $TMPDIR/rancher/containerlogs/agent-$RANCHERAGENT 2>&1
+    docker logs $SINCE_FLAG -t $RANCHERAGENT | sed 's/token.*/token REDACTED/g' > $TMPDIR/rancher/containerlogs/agent-$RANCHERAGENT 2>&1
   done
 
   # K8s Docker container logging
