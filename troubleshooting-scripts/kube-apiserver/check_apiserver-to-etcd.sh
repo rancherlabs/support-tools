27af9838123a2d707f53108dfde7762040e7975f
Matthew Mattox <cube8021@gmail.com>
2020-09-16 14:58:26 -0500

    Update troubleshooting-scripts/kube-apiserver/check_apiserver-to-etcd.sh
    Co-authored-by: dkeightley <20566450+dkeightley@users.noreply.github.com>

--- troubleshooting-scripts/kube-apiserver/check_apiserver-to-etcd.sh
+++ troubleshooting-scripts/kube-apiserver/check_apiserver-to-etcd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(docker exec -it kube-apiserver sh -c 'ps aux | grep kube-apiserver' | awk -F'--etcd-servers' '{print $2}' | awk -F ' ' '{print $1}' | tr -d '=' | tr , "\n" | sed '/^$/d' | awk -F '/' '{print $3}' | awk -F ':' '{print $1}')
+for i in $(docker inspect kube-apiserver | grep -m 1 "\--etcd-servers" | grep -Po '(?<=https://)[^:]*')
 do
   echo -n "Checking $i "
   curl -k -X GET --cacert /etc/kubernetes/ssl/kube-ca.pem --cert /etc/kubernetes/ssl/kube-node.pem --key /etc/kubernetes/ssl/kube-node-key.pem https://"$i":2379/health
