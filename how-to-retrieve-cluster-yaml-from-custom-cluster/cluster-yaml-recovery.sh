8d3198bbfc7adf435a7f7486197ad41582b33141
Matthew Mattox <matt.mattox@suse.com>
2021-02-08 20:44:03 -0600

    Adding script for recoverying a cluster.yaml using kubectl.

---
+++ how-to-retrieve-cluster-yaml-from-custom-cluster/cluster-yaml-recovery.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [[ ! -f kube_config_cluster.yml ]]
+then
+  echo "The kube_config_cluster.yml is missing"
+  exit 1
+fi
+
+echo "Building cluster.yml..."
+echo "Working on Nodes..."
+echo 'nodes:' > cluster.yml
+kubectl --kubeconfig kube_config_cluster.yml -n kube-system get configmap full-cluster-state -o json | jq -r .data.\"full-cluster-state\" | jq -r .desiredState.rkeConfig.nodes | yq r - | sed 's/^/  /' | \
+sed -e 's/internalAddress/internal_address/g' | \
+sed -e 's/hostnameOverride/hostname_override/g' | \
+sed -e 's/sshKeyPath/ssh_key_path/g' >> cluster.yml
+echo "" >> cluster.yml
+
+echo "Working on services..."
+echo 'services:' >> cluster.yml
+kubectl --kubeconfig kube_config_cluster.yml -n kube-system get configmap full-cluster-state -o json | jq -r .data.\"full-cluster-state\" | jq -r .desiredState.rkeConfig.services | yq r - | sed 's/^/  /' >> cluster.yml
+echo "" >> cluster.yml
+
+echo "Working on network..."
+echo 'network:' >> cluster.yml
+kubectl --kubeconfig kube_config_cluster.yml -n kube-system get configmap full-cluster-state -o json | jq -r .data.\"full-cluster-state\" | jq -r .desiredState.rkeConfig.network | yq r - | sed 's/^/  /' >> cluster.yml
+echo "" >> cluster.yml
+
+echo "Working on authentication..."
+echo 'authentication:' >> cluster.yml
+kubectl --kubeconfig kube_config_cluster.yml -n kube-system get configmap full-cluster-state -o json | jq -r .data.\"full-cluster-state\" | jq -r .desiredState.rkeConfig.authentication | yq r - | sed 's/^/  /' >> cluster.yml
+echo "" >> cluster.yml
+
+echo "Working on systemImages..."
+echo 'system_images:' >> cluster.yml
+kubectl --kubeconfig kube_config_cluster.yml -n kube-system get configmap full-cluster-state -o json | jq -r .data.\"full-cluster-state\" | jq -r .desiredState.rkeConfig.systemImages | yq r - | sed 's/^/  /' >> cluster.yml
+echo "" >> cluster.yml
+
+echo "Building cluster.rkestate..."
+kubectl --kubeconfig kube_config_cluster.yml -n kube-system get configmap full-cluster-state -o json | jq -r .data.\"full-cluster-state\" | jq -r . > cluster.rkestate
+
+read -n1 -rsp $'Press any key to continue run an rke up or Ctrl+C to exit...\n'
+echo "Running rke up..."
+rke up --config cluster.yml
