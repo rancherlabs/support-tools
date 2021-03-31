8bc9bd57aea02ebd9568b6c6d5507148d668043d
dkeightley <derek@rancher.com>
2021-04-01 11:58:14 +1300

    Remove subdirectories only in /var/lib/kubelet

--- extended-rancher-2-cleanup/extended-cleanup-rancher2.sh
+++ extended-rancher-2-cleanup/extended-cleanup-rancher2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directories to cleanup
-CLEANUP_DIRS=(/etc/ceph /etc/cni /etc/kubernetes /opt/cni /opt/rke /run/secrets/kubernetes.io /run/calico /run/flannel /var/lib/calico /var/lib/weave /var/lib/etcd /var/lib/cni /var/lib/kubelet /var/lib/rancher/rke/log /var/log/containers /var/log/pods /var/run/calico)
+CLEANUP_DIRS=(/etc/ceph /etc/cni /etc/kubernetes /opt/cni /opt/rke /run/secrets/kubernetes.io /run/calico /run/flannel /var/lib/calico /var/lib/weave /var/lib/etcd /var/lib/cni /var/lib/kubelet/* /var/lib/rancher/rke/log /var/log/containers /var/log/pods /var/run/calico)
 
 # Interfaces to cleanup
 CLEANUP_INTERFACES=(flannel.1 cni0 tunl0)
