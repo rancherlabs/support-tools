c4326e86a7f5c13e1ac1ef5334e7cf4ddacaacff
Matthew Mattox <matt.mattox@suse.com>
2021-09-21 14:03:11 -0500

    Update cluster-yaml-recovery.sh

--- how-to-retrieve-cluster-yaml-from-custom-cluster/cluster-yaml-recovery.sh
+++ how-to-retrieve-cluster-yaml-from-custom-cluster/cluster-yaml-recovery.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 verify-access() {
@@ -64,140 +65,19 @@ checks() {
 }
 
 build() {
-echo "Building cluster.yml..."
-${KUBECTL_CMD} -n kube-system get configmap full-cluster-state -o json | jq -r .data.\"full-cluster-state\" | jq -r .desiredState.rkeConfig | yq r -P - | sed 's/^/  /' | \
-sed -e 's/internalAddress/internal_address/g' | \
-sed -e 's/hostnameOverride/hostname_override/g' | \
-sed -e 's/dockerSocket/docker_socket/g' | \
-sed -e 's/sshAgentAuth/ssh_agent_auth/g' | \
-sed -e 's/sshKey/ssh_key/g' | \
-sed -e 's/sshKeyPath/ssh_key_path/g' | \
-sed -e 's/sshCert/ssh_cert/g' | \
-sed -e 's/sshCertPath/ssh_cert_path/g' | \
-sed -e 's/kubeApi/kube-api/g' | \
-sed -e 's/kubeController/kube-controller/g' | \
-sed -e 's/externalUrls/external_urls/g' | \
-sed -e 's/caCert/ca_cert/g' | \
-sed -e 's/backupConfig/backup_config/g' | \
-sed -e 's/serviceClusterIpRange/service_cluster_ip_range/g' | \
-sed -e 's/serviceNodePortRange/service_node_port_range/g' | \
-sed -e 's/podSecurityPolicy/pod_security_policy/g' | \
-sed -e 's/alwaysPullImages/always_pull_images/g' | \
-sed -e 's/SecretsEncryptionConfig /secrets_encryption_config/g' | \
-sed -e 's/auditLog/ssh_key_path/g' | \
-sed -e 's/auditLog/audit_log/g' | \
-sed -e 's/admissionConfiguration/admission_configuration/g' | \
-sed -e 's/eventRateLimit/event_rate_limit/g' | \
-sed -e 's/maxAge/max_age/g' | \
-sed -e 's/maxBackup/max_backup/g' | \
-sed -e 's/maxSize/max_size/g' | \
-sed -e 's/clusterCidr/cluster_cidr/g' | \
-sed -e 's/serviceClusterIpRange/service_cluster_ip_range/g' | \
-sed -e 's/clusterDomain/cluster_domain/g' | \
-sed -e 's/infraContainerImage/infra_container_image/g' | \
-sed -e 's/clusterDnsServer/cluster_dns_server/g' | \
-sed -e 's/failSwapOn/fail_swap_on/g' | \
-sed -e 's/generate_serving_certificate/generateServingCertificate/g' | \
-sed -e 's/extraArgs/extra_args/g' | \
-sed -e 's/extraBinds/extra_binds/g' | \
-sed -e 's/extraEnv/extraEnv/g' | \
-sed -e 's/winExtraArgs/win_extra_args/g' | \
-sed -e 's/winExtraBinds/win_extra_binds/g' | \
-sed -e 's/winExtraEnv/win_extra_env/g' | \
-sed -e 's/calicoNetworkProvider/calico_network_provider/g' | \
-sed -e 's/canalNetworkProvider/canal_network_provider/g' | \
-sed -e 's/flannelNetworkProvider/flannel_network_provider/g' | \
-sed -e 's/weaveNetworkProvider/weave_network_provider/g' | \
-sed -e 's/aciNetworkProvider/aci_network_provider/g' | \
-sed -e 's/nodeSelector/node_selector/g' | \
-sed -e 's/updateStrategy/update_strategy/g' | \
-sed -e 's/configFile/config_file/g' | \
-sed -e 's/cacheTimeout/cache_timeout/g' | \
-sed -e 's/nodeSelector/node_selector/g' | \
-sed -e 's/dns_policy/dnsPolicy/g' | \
-sed -e 's/extraVolumes/extra_volumes/g' | \
-sed -e 's/extraVolumeMounts/extra_volume_mounts/g' | \
-sed -e 's/httpPort/http_port/g' | \
-sed -e 's/httpsPort/https_port/g' | \
-sed -e 's/networkMode/network_mode/g' | \
-sed -e 's/defaultBackend/default_backend/g' | \
-sed -e 's/defaultHttpBackendPriorityClassName/default_http_backend_priority_class_name/g' | \
-sed -e 's/nginx_ingress_controller_priority_class_name/nginxIngressControllerPriorityClassName/g' | \
-sed -e 's/systemId/system_id/g' | \
-sed -e 's/apicHosts/apic_hosts/g' | \
-sed -e 's/apicUserName/apic_user_name/g' | \
-sed -e 's/apicUserKey/apic_user_key/g' | \
-sed -e 's/apicUserCrt/apic_user_crt/g' | \
-sed -e 's/apicRefreshTime/apic_refresh_time/g' | \
-sed -e 's/vmmDomain/vmm_domain/g' | \
-sed -e 's/vmmController/vmm_controller/g' | \
-sed -e 's/encapType/encap_type/g' | \
-sed -e 's/nodeSubnet/node_subnet/g' | \
-sed -e 's/mcastRangeStart/mcast_range_start/g' | \
-sed -e 's/mcastRangeEnd/mcast_range_end/g' | \
-sed -e 's/vrfName/vrf_name/g' | \
-sed -e 's/vrfTenant/vrf_tenant/g' | \
-sed -e 's/l3outExternalNetworks/l3out_external_networks/g' | \
-sed -e 's/externDynamic/extern_dynamic/g' | \
-sed -e 's/externStatic/extern_static/g' | \
-sed -e 's/nodeSvcSubnet/node_svc_subnet/g' | \
-sed -e 's/kubeApiVlan/kube_api_vlan/g' | \
-sed -e 's/serviceVlan/service_vlan/g' | \
-sed -e 's/infraVlan/infra_vlan/g' | \
-sed -e 's/ovsMemoryLimit/ovs_memory_limit/g' | \
-sed -e 's/imagePullPolicy/image_pull_policy/g' | \
-sed -e 's/imagePullSecret/image_pull_secret/g' | \
-sed -e 's/serviceMonitorInterval/service_monitor_interval/g' | \
-sed -e 's/pbrTrackingNonSnat/pbr_tracking_non_snat/g' | \
-sed -e 's/installIstio/install_istio/g' | \
-sed -e 's/istioProfile/istio_profile/g' | \
-sed -e 's/dropLogEnable/drop_log_enable/g' | \
-sed -e 's/controllerLogLevel/controller_log_level/g' | \
-sed -e 's/hostAgentLogLevel/host_agent_log_level/g' | \
-sed -e 's/opflexLogLevel/opflex_log_level/g' | \
-sed -e 's/useAciCniPriorityClass/use_aci_cni_priority_class/g' | \
-sed -e 's/noPriorityClass/no_priority_class/g' | \
-sed -e 's/maxNodesSvcGraph/max_nodes_svc_graph/g' | \
-sed -e 's/snatContractScope/snat_contract_scope/g' | \
-sed -e 's/podSubnetChunkSize/pod_subnet_chunk_size/g' | \
-sed -e 's/enableEndpointSlice/enable_endpoint_slice/g' | \
-sed -e 's/snatNamespace/snat_namespace/g' | \
-sed -e 's/epRegistry/ep_registry/g' | \
-sed -e 's/opflexMode/opflex_mode/g' | \
-sed -e 's/snatPortRangeStart/snat_port_range_start/g' | \
-sed -e 's/snatPortRangeEnd/snat_port_range_end/g' | \
-sed -e 's/snatPortsPerNode/snat_ports_per_node/g' | \
-sed -e 's/opflexClientSsl/opflex_client_ssl/g' | \
-sed -e 's/usePrivilegedContainer/use_privileged_container/g' | \
-sed -e 's/useHostNetnsVolume/use_host_netns_volume/g' | \
-sed -e 's/useOpflexServerVolume/use_opflex_server_volume/g' | \
-sed -e 's/subnetDomainName/subnet_domain_name/g' | \
-sed -e 's/kafkaBrokers/kafka_brokers/g' | \
-sed -e 's/kafkaClientCrt/kafka_client_crt/g' | \
-sed -e 's/kafkaClientKey/kafka_client_key/g' | \
-sed -e 's/useAciAnywhereCrd/use_aci_anywhere_crd/g' | \
-sed -e 's/overlayVrfName/overlay_vrf_name/g' | \
-sed -e 's/gbpPodSubnet/gbp_pod_subnet/g' | \
-sed -e 's/runGbpContainer/run_gbp_container/g' | \
-sed -e 's/runOpflexServerContainer/run_opflex_server_container/g' | \
-sed -e 's/opflexServerPort/opflex_server_port/g' | \
-sed -e 's/virtualCenter/virtual_center/g' | \
-sed -e 's/loadBalancer/load_balancer/g' | \
-sed -e 's/blockStorage/block_storage/g' | \
-sed -e 's/serviceOverride/service_override/g' | \
-sed -e 's/metricsServerPriorityClassName/metrics_server_priority_class_name/g' | \
-sed -e 's/snapshotName/snapshot_name/g' | \
-sed -e 's/linearAutoscalerParams/linear_autoscaler_params/g' | \
-sed -e 's/ipAddress/ip_address/g' | \
-sed -e 's/nodeLocalDnsPriorityClassName/node_local_dns_priority_class_name/g' | \
-sed -e 's/coresPerReplica/cores_per_replica/g' | \
-sed -e 's/nodesPerReplica/nodes_per_replica/g' | \
-sed -e 's/preventSinglePointFailure/prevent_single_point_failure/g' | \
-sed -e 's/customConfig/custom_config/g' | \
-sed -e 's/ignoreDaemonSets/ignore_daemonsets/g' | \
-sed -e 's/deleteLocalData/delete_local_data/g' | \
-sed -e 's/gracePeriod/grace_period/g' > cluster.yml
-echo "" >> cluster.yml
+ echo "Building cluster.yml..."
+ IFS=''
+ while read line
+ do
+  word=$(echo "$line" |awk -F ":" '{print $1}')
+  new_word=$(echo "$line" |awk -F ":" '{print $1}' | perl -pe 's/([a-z0-9])([A-Z])/$1_\L$2/g')
+  echo "$line"| sed "s/$word/$new_word/g" >>cluster.yml
+ done< <(${KUBECTL_CMD} -n kube-system get configmap full-cluster-state -o json | jq -r .data.\"full-cluster-state\" | jq -r .desiredState.rkeConfig | yq r -P -)
+
+ echo "Building cluster.rkestate..."
+ ${KUBECTL_CMD} -n kube-system get configmap full-cluster-state -o json | jq -r .data.\"full-cluster-state\" | jq -r . > cluster.rkestate
+}
+
 
 echo "Building cluster.rkestate..."
 ${KUBECTL_CMD} -n kube-system get configmap full-cluster-state -o json | jq -r .data.\"full-cluster-state\" | jq -r . > cluster.rkestate
