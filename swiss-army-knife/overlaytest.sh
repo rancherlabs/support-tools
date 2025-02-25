b4c53497e91ced15b4904783292b11139784dd56
dkeightley <derek@rancher.com>
2025-02-26 12:17:45 +1300

    Use default namespace, remove timeout, add stats

--- swiss-army-knife/overlaytest.sh
+++ swiss-army-knife/overlaytest.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-DNS_CHECK=false
+DNS_TEST=false
+NAMESPACE=default
 
 # Parse arguments
 while [[ $# -gt 0 ]]; do
   case $1 in
-    --dns-check)
-      DNS_CHECK=true
+    --dns-test)
+      DNS_TEST=true
       shift
       ;;
     *)
@@ -16,53 +17,63 @@ while [[ $# -gt 0 ]]; do
   esac
 done
 
-echo "=> Start network overlay, DNS, and API test"
+echo "=> Start network overlay and DNS test"
+if $DNS_TEST
+  then
+    DNS_PASS=0; DNS_FAIL=0
+  else
+    echo "DNS tests are skipped. Use --dns-check to enable."
+fi
+echo
+NET_PASS=0; NET_FAIL=0
 
-kubectl get pods -l name=overlaytest -o jsonpath='{range .items[*]}{@.metadata.name}{" "}{@.spec.nodeName}{" "}{@.status.podIP}{"\n"}{end}' | sort -k2 |
 while read spod shost sip
 do
   echo "Testing pod $spod on node $shost with IP $sip"
 
   # Overlay network test
   echo "  => Testing overlay network connectivity"
-  kubectl get pods -l name=overlaytest -o jsonpath='{range .items[*]}{@.status.podIP}{" "}{@.spec.nodeName}{"\n"}{end}' | sort -k2 |
-  while read tip thost
+    while read tip thost
   do
     if [[ ! $shost == $thost ]]; then
-      kubectl --request-timeout='10s' exec $spod -c overlaytest -- /bin/sh -c "ping -c2 $tip > /dev/null 2>&1"
+      kubectl -n $NAMESPACE exec $spod -c overlaytest -- /bin/sh -c "ping -c2 $tip > /dev/null 2>&1"
       RC=$?
       if [ $RC -ne 0 ]; then
-        echo "    FAIL: $spod on $shost cannot reach pod IP $tip on $thost"
+        ((NET_FAIL+=1)); echo "    FAIL: $spod on $shost cannot reach pod IP $tip on $thost"
       else
-        echo "    PASS: $spod on $shost can reach pod IP $tip on $thost"
+        ((NET_PASS+=1)); echo "    PASS: $spod on $shost can reach pod IP $tip on $thost"
       fi
     fi
-  done
+  done < <(kubectl get pods -n $NAMESPACE -l name=overlaytest -o jsonpath='{range .items[*]}{@.status.podIP}{" "}{@.spec.nodeName}{"\n"}{end}' | sort -k2)
 
-  if $DNS_CHECK; then
+  if $DNS_TEST; then
     # Internal DNS test
-    echo "  => Testing internal DNS"
-    kubectl --request-timeout='10s' exec $spod -c overlaytest -- /bin/sh -c "nslookup kubernetes.default > /dev/null 2>&1"
+    echo "  => Testing DNS"
+    kubectl -n $NAMESPACE exec $spod -c overlaytest -- /bin/sh -c "nslookup kubernetes.default > /dev/null 2>&1"
     RC=$?
     if [ $RC -ne 0 ]; then
-      echo "    FAIL: $spod cannot resolve internal DNS for 'kubernetes.default'"
+      ((DNS_FAIL+=1)); echo "    FAIL: $spod cannot resolve internal DNS for 'kubernetes.default'"
     else
-      echo "    PASS: $spod can resolve internal DNS for 'kubernetes.default'"
+      ((DNS_PASS+=1)); echo "    PASS: $spod can resolve internal DNS for 'kubernetes.default'"
     fi
 
     # External DNS test
-    echo "  => Testing external DNS"
-    kubectl --request-timeout='10s' exec $spod -c overlaytest -- /bin/sh -c "nslookup rancher.com > /dev/null 2>&1"
+    kubectl -n $NAMESPACE exec $spod -c overlaytest -- /bin/sh -c "nslookup rancher.com > /dev/null 2>&1"
     RC=$?
     if [ $RC -ne 0 ]; then
-      echo "    FAIL: $spod cannot resolve external DNS for 'rancher.com'"
+      ((DNS_FAIL+=1)); echo "    FAIL: $spod cannot resolve external DNS for 'rancher.com'"
     else
-      echo "    PASS: $spod can resolve external DNS for 'rancher.com'"
+      ((DNS_PASS+=1)); echo "    PASS: $spod can resolve external DNS for 'rancher.com'"
     fi
-  else
-    echo "  => DNS checks are skipped. Use --dns-check to enable."
   fi
+  echo
 
-done
+done < <(kubectl get pods -n $NAMESPACE -l name=overlaytest -o jsonpath='{range .items[*]}{@.metadata.name}{" "}{@.spec.nodeName}{" "}{@.status.podIP}{"\n"}{end}' | sort -k2)
 
-echo "=> End network overlay, DNS, and API test"
+NET_TOTAL=$(($NET_PASS + $NET_FAIL))
+echo "=> Network [$NET_PASS / $NET_TOTAL]"
+if $DNS_TEST; then
+  DNS_TOTAL=$(($DNS_PASS + $DNS_FAIL))
+  echo "=> DNS     [$DNS_PASS / $DNS_TOTAL]"
+fi
+echo; echo "=> End network overlay and DNS test"
